c162748136a903953f20400a695ecd79a6ea9451
Tomasz63770 <tomasz63770@gmail.com>
2026-06-27 17:38:27 +0200

    Add files via upload

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,14 @@
 if [ "$1" == "--date" ]; then
   date
 fi
+
+if [ "$1" == "--logs" ]; then
+  for i in $(seq 1 100); do
+    echo "log$i.txt $(date) skrypt.sh" > "log$i.txt"
+  done
+fi
+
+if [ "$1" == "--help" ]; then
+  echo "--date  - pokazuje datę"
+  echo "--logs  - tworzy pliki log"
+fi
